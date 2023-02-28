2f61ec994376770734e34eb3610a393ebe82da0b
Ryan Schmidt <blueboard2014@aol.com>
2023-02-27 18:51:55 -0700

    work on the new task form

--- Site-Stuff/Scripting/Snippets/template_level_snippets.js
+++ Site-Stuff/Scripting/Snippets/template_level_snippets.js
@@ -5,7 +5,7 @@ function addHTMLSnippet(id, html) {
 let currentUrl = window.location.href;
 if(currentUrl.split("").pop() == "/"){
     homeUrl = currentUrl.slice(0,-1);
-    homeUrl += "_Home_Page";
+    homeUrl += "_Home";
 };
 if(typeof homeUrl === "string"){ var currentFilename = homeUrl.split("/").pop().split('.')[0].replaceAll("_", " ");
 } else { var currentFilename = currentUrl.split("/").pop().split('.')[0].replaceAll("_", " ");};
@@ -19,7 +19,7 @@ const lastModified = new Date(document.lastModified);
 // html snippets
 let htmlHeadCharset = '<!--Keep updated, modern standard--><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0">';
 let htmlHeadCSS = '<!-- Cascading Style Sheets--><link rel="stylesheet" type="text/css" href="/Site-Stuff/Styles/phone.css" media="screen"/><!-- ADD NORMALIZE CSS HERE, then others starting with base and then working outward to the largest view -->';
-let htmlHeaderSiteWideNav = `<ul><a href="#"><li>Library</li></a><a href="/Family/"><li>Family</li></a><a href="/"><li>Home</li></a><a href="#"><li>Kids</li></a><a href="#"><li>About</li></a></ul><h1>Welcome to My ${currentFilename} !</h1>`;
+let htmlHeaderSiteWideNav = `<ul><a href="#"><li>Library</li></a><a href="/Family/"><li>Family</li></a><a href="/"><li>Home</li></a><a href="/Kids/"><li>Kids</li></a><a href="#"><li>About</li></a></ul><h1>Welcome to R ${currentFilename} Page!</h1>`;
 let htmlMainFooter = `&copy; Gunnar Schmidt ${months[lastModified.getMonth()]} ${lastModified.getFullYear()}`;
 
 //   <!--Page Data-->hjfZuhj  
